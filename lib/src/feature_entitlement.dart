2cefd50a82b4cd648f81b2d1f3dbc9bf28d5d744
Huzaif Mushtaq Mir <huzaif@Huzaif-MacBook-Air.local>
2026-03-09 14:25:14 +0530

    feat: add base value to fe

--- lib/src/feature_entitlement.dart
+++ lib/src/feature_entitlement.dart
@@ -4,12 +4,14 @@ class FeatureEntitlement {
   final String featureName;
   final String featureDisplayName;
   final String value;
+  final String baseValue;
   final int expiresAt;
 
   FeatureEntitlement({
     required this.featureName,
     required this.featureDisplayName,
     required this.value,
+    required this.baseValue,
     required this.expiresAt,
   });
 
@@ -18,6 +20,7 @@ class FeatureEntitlement {
         featureName: json['featureName'] ?? '',
         featureDisplayName: json['featureDisplayName'] ?? '',
         value: json['value'] ?? '',
+        baseValue: json['baseValue'] ?? '',
         expiresAt: json['expiresAt'] ?? 0,
     );
   }
